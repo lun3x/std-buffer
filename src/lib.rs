aba0de2b045ff9d2d30cddffa5e76fe2301226d2
Louis Wyborn <louiswyborn@gmail.com>
2026-01-04 13:22:23 +0100

    Pull changes from std, bump to 0.3.0, bump toolchain

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(maybe_uninit_slice)]
-#![feature(new_uninit)]
 #![feature(read_buf)]
 #![feature(core_io_borrowed_buf)]
+#![feature(allocator_api)]
+#![feature(io_const_error)]
 mod buffer;
 pub use buffer::Buffer;
